7035be40886514b8e8edcd7d93af4d0fb8d47ef8
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-10 01:38:23 +0700

    fix: docker

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('🔥');
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    `<h1 style="width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; font-size: 144px">
+    🔥
+    </h1>`
+  );
 });
 
 app.listen(port, () => {
