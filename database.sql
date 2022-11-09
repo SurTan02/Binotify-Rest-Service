3f221ebf2e6ba2940bde991e3c7751eb97eb41e2
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-09 20:53:44 +0700

    chore: set up database

---
+++ database.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS `database`;
+
+CREATE DATABASE `database`;
+
+CREATE TABLE `database`.`User`
+(
+    `user_id`   INT,
+    `email`     VARCHAR(256) NOT NULL,
+    `password`  VARCHAR(256) NOT NULL,
+    `username`  VARCHAR(256) NOT NULL,
+    `name`      VARCHAR(256) NOT NULL,
+    `isAdmin`   BOOLEAN      NOT NULL DEFAULT 0,
+    PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `database`.`Song`
+(
+    `song_id`       INT,
+    `judul`         VARCHAR(64)  NOT NULL,
+    `penyanyi_id`   INT          NOT NULL,
+    `audio_path`    VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`song_id`),
+    FOREIGN KEY (`penyanyi_id`) REFERENCES `User` (`user_id`)
+);
