d7812bf057864c23d13ff8ee0202c726800c81fb
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-19 00:57:33 +0700

    feat: register route

---
+++ src/routes/register.route.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import * as db from "../db/connection";
+import hash from "../helper/hash.helper";
+
+const router = Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, username, name } = req.body;
+
+    const _ = await db.execute(
+      `INSERT INTO User (email, password, username, name) VALUE (?, ?, ?, ?)`,
+      [email, hash(password), username, name]
+    );
+
+    return res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+});
+
+export { router as register };
