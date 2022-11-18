a957b0eceab656b73b8e2bf63852a750ec730686
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-19 00:57:01 +0700

    fix: database `User` table type

--- database.sql
+++ database.sql
@@ -4,13 +4,15 @@ CREATE DATABASE `database`;
 
 CREATE TABLE `database`.`User`
 (
-    `user_id`   INT,
+    `user_id`   INT AUTO_INCREMENT,
     `email`     VARCHAR(256) NOT NULL,
     `password`  VARCHAR(256) NOT NULL,
     `username`  VARCHAR(256) NOT NULL,
     `name`      VARCHAR(256) NOT NULL,
     `isAdmin`   BOOLEAN      NOT NULL DEFAULT 0,
-    PRIMARY KEY (`user_id`)
+    PRIMARY KEY (`user_id`),
+    UNIQUE(`email`),
+    UNIQUE(`username`)
 );
 
 CREATE TABLE `database`.`Song`
