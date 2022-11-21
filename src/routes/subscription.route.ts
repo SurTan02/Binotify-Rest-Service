f168f13bd88c23c10f22a0387d0f989c02713fd1
Suryanto Tan <suryanto.tan5@gmail.com>
2022-11-21 21:17:00 +0700

    feat: add getRequest from Soap + convert xml-json

--- src/routes/subscription.route.ts
+++ src/routes/subscription.route.ts
@@ -4,9 +4,11 @@ import {
   authenticateToken,
   authorizeRole,
 } from "../middleware/auth.middleware";
+import { transform } from "camaro";
 
 const router = Router();
 
+// Get subscribe request
 router.get("/subscription", async (req, res) => {
   /* CODE HERE */
   let xmls =
@@ -14,18 +16,36 @@ router.get("/subscription", async (req, res) => {
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.binotifysoap.com/">\
         <soapenv:Header/>\
         <soapenv:Body>\
-          <ser:addSubscription>\
-            <arg0>1</arg0>\
-            <arg1>2</arg1>\
-          </ser:addSubscription>\
+          <ser:getSubscription>\
+          </ser:getSubscription>\
       </soapenv:Body>\
     </soapenv:Envelope>';
 
   const result = await soapConsumer(xmls);
-  console.log(result);
   
+  const template = {
+    subscription: ['//subscription', {
+        creator_id: 'creatorId',
+        subscriber_id: 'subscriberId',
+        status: 'status'
+    }]
+  }
 
-  return res.send(result);
+  let jsonResult = await transform((<any>result).response.body, template);
+  return res.send(JSON.stringify(jsonResult, null, 2));
 });
 
 export { router as subscription };
+
+
+// ADD Request, tar dipake di php
+// '\
+//     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.binotifysoap.com/">\
+//         <soapenv:Header/>\
+//         <soapenv:Body>\
+//           <ser:addSubscription>\
+//             <arg0>1</arg0>\
+//             <arg1>2</arg1>\
+//           </ser:addSubscription>\
+//       </soapenv:Body>\
+//     </soapenv:Envelope>';
