6328d9db7679275f860cfc2b54b6b8717996dbcf
Suryanto Tan <suryanto.tan5@gmail.com>
2022-11-24 22:47:58 +0700

    feat: edit template to handle pagination

--- src/config/template.config.ts
+++ src/config/template.config.ts
@@ -1,7 +1,11 @@
 export const subscription_template = {
-    subscription: ['//subscription', {
-        creator_id: 'number(creatorId)',
-        subscriber_id: 'number(subscriberId)',
-        status: 'status'
+    subscriptions: ['//Subscriptions', {
+        current: 'number(current_page)',
+        total: 'number(total_pages)',
+        subscription: ['//subscription', {
+            creator_id: 'number(creatorId)',
+            subscriber_id: 'number(subscriberId)',
+            status: 'status'
+        }]
     }]
 }
