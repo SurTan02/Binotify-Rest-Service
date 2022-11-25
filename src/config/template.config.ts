bd521d63c6885c9064dce46eabfea46a36e31c63
Suryanto Tan <suryanto.tan5@gmail.com>
2022-11-25 23:01:38 +0700

    feat: add updateStatus endpoint

--- src/config/template.config.ts
+++ src/config/template.config.ts
@@ -1,7 +1,7 @@
 export const subscription_template = {
     subscriptions: ['//Subscriptions', {
-        current: 'number(current_page)',
-        total: 'number(total_pages)',
+        current: 'number(currentPage)',
+        total: 'number(totalPages)',
         subscription: ['//subscription', {
             creator_id: 'number(creatorId)',
             subscriber_id: 'number(subscriberId)',
--- src/routes/subscription.route.ts
+++ src/routes/subscription.route.ts
@@ -7,23 +7,50 @@ import {
 import { subscription_template } from "../config/template.config";
 
 
+
 const router = Router();
 
 // Get subscribe request
 router.get("/subscription", async (req, res) => {
   /* CODE HERE */
+  var current = 1;
+  if (req.query.page){
+    current = parseInt(req.query.page as string); 
+  }
+
   let xmls =
-    '\
+    `\
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.binotifysoap.com/">\
         <soapenv:Header/>\
         <soapenv:Body>\
-          <ser:getSubscription>\
-          </ser:getSubscription>\
+          <ser:GetSubscription>\
+            <current_page>${current}</current_page>\
+          </ser:GetSubscription>\
       </soapenv:Body>\
-    </soapenv:Envelope>';
+    </soapenv:Envelope>`;
 
   const result = await getAsJson(xmls, subscription_template);
   return res.send(result);
+});
+
+// Update subscription
+router.patch("/subscription", async (req, res) => {
+  /* CODE HERE */
+  const { creator_id, subscriber_id, status} = req.body;
+  let xmls =
+    `\
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.binotifysoap.com/">\
+        <soapenv:Header/>\
+        <soapenv:Body>\
+          <ser:UpdateSubscription>\
+            <creator_id>${creator_id}</creator_id>\
+            <subscriber_id>${subscriber_id}</subscriber_id>\
+            <status>${status}</status>\
+          </ser:UpdateSubscription>\
+      </soapenv:Body>\
+    </soapenv:Envelope>`;
+  
+    const result = await soapConsumer(xmls);
 });
 
 export { router as subscription };
