0b62cec3af84c9d98b4a326eb96e5a41e9f70f5b
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-26 10:41:28 +0700

    fix: CORS for '/' endpoint

--- src/config/vars.config.ts
+++ src/config/vars.config.ts
@@ -3,20 +3,28 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const DATA_SOURCES = {
-  host              : process.env.BINOTIFY_REST_SERVICE_DB_HOST     || 'binotify-rest-service-db',
-  user              : process.env.BINOTIFY_REST_SERVICE_DB_USER     || 'user',
-  password          : process.env.BINOTIFY_REST_SERVICE_DB_PASSWORD || 'password',
-  database          : process.env.BINOTIFY_REST_SERVICE_DB_NAME     || 'database',
-  port              : 3306,
+  host: process.env.BINOTIFY_REST_SERVICE_DB_HOST || "binotify-rest-service-db",
+  user: process.env.BINOTIFY_REST_SERVICE_DB_USER || "user",
+  password: process.env.BINOTIFY_REST_SERVICE_DB_PASSWORD || "password",
+  database: process.env.BINOTIFY_REST_SERVICE_DB_NAME || "database",
+  port: 3306,
   waitForConnections: true,
-  connectionLimit   : 10,
-  queueLimit        : 0
+  connectionLimit: 10,
+  queueLimit: 0,
 };
 
 export const PORT = process.env.BINOTIFY_APP_WEB_PORT || 8080;
 
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'rahasia';
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "rahasia";
 
-export const PW_HASH_TYPE = process.env.PW_HASH_TYPE || 'sha256';
+export const PW_HASH_TYPE = process.env.PW_HASH_TYPE || "sha256";
 
-export const SOAP_SERVICE_URL = process.env.SOAP_SERVICE_URL || 'http://binotify-soap-service:8081/com/binotifysoap/SubscriptionService';
+export const SOAP_SERVICE_URL =
+  process.env.SOAP_SERVICE_URL ||
+  "http://binotify-soap-service:8081/com/binotifysoap/SubscriptionService";
+
+export const corsOptions = {
+  origin: process.env.CLIENT_HOST ||"http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import express, { Express, Request, Response } from "express";
-import { PORT } from "./config/vars.config";
+import { corsOptions, PORT } from "./config/vars.config";
 import * as db from "./db/connection";
 import { routes } from "./routes/routes";
-var cors = require('cors')
-
-const corsOptions ={
-  origin:'http://localhost:5173', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-
+import cors from "cors";
 
 db.init();
 
 const app: Express = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
   res.send(
@@ -25,7 +19,6 @@ app.get("/", (req: Request, res: Response) => {
   );
 });
 
-app.use(cors(corsOptions));
 app.use(routes);
 
 app.listen(PORT, async () => {
