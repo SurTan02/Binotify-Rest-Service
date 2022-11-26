03fb83f34614e93819216a91ec6f18ed61c59598
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-26 13:47:41 +0700

    feat: add validate username and email

--- src/routes/register.route.ts
+++ src/routes/register.route.ts
@@ -1,6 +1,9 @@
+import { IUser } from "./../model/user.model";
 import { Router } from "express";
 import * as db from "../db/connection";
 import hash from "../helper/hash.helper";
+import { sign } from "jsonwebtoken";
+import { ACCESS_TOKEN_SECRET } from "../config/vars.config";
 
 const router = Router();
 
@@ -12,6 +15,57 @@ router.post("/register", async (req, res) => {
       `INSERT INTO User (email, password, username, name) VALUE (?, ?, ?, ?)`,
       [email, hash(password), username, name]
     );
+
+    const account: IUser[] = await db.execute(
+      `SELECT * FROM User WHERE username = ? AND password = ?`,
+      [username, hash(password)]
+    );
+
+    const accessToken = sign(
+      {
+        user_id: account[0].user_id,
+        username: username,
+        isAdmin: account[0].isAdmin,
+      },
+      ACCESS_TOKEN_SECRET
+    );
+
+    return res.json({
+      accessToken: "Bearer " + accessToken,
+      isAdmin: account[0].isAdmin,
+    });
+  } catch (e) {
+    return res.sendStatus(403);
+  }
+});
+
+router.get("/register/email/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+
+    const result: IUser[] = await db.execute(
+      `SELECT * FROM User WHERE email = ?`,
+      [email]
+    );
+
+    if (result.length > 0) return res.sendStatus(403);
+
+    return res.sendStatus(200);
+  } catch {
+    return res.sendStatus(500);
+  }
+});
+
+router.get("/register/username/:username", async (req, res) => {
+  try {
+    const username = req.params.username;
+
+    const result: IUser[] = await db.execute(
+      `SELECT * FROM User WHERE username = ?`,
+      [username]
+    );
+
+    if (result.length > 0) return res.sendStatus(403);
 
     return res.sendStatus(200);
   } catch {
