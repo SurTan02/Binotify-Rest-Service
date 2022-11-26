00f8b604159f9a9ffd4958d18b9d4c79abc48e07
fikrikhoironn <fikrikf470@gmail.com>
2022-11-26 19:49:07 +0700

    feat: get user in rest 2

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import { login } from "./login.route";
 import { register } from "./register.route";
 import { song } from "./song.route";
 import { subscription } from "./subscription.route";
+import { user } from "./user.route";
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use(login);
 routes.use(example);
 routes.use(song);
 routes.use(subscription);
+routes.use(user);
 
 export { routes };
---
+++ src/routes/user.route.ts
@@ -0,0 +1,23 @@
+import { IUser } from "./../model/user.model";
+import { Router } from "express";
+import * as db from "../db/connection";
+import { authenticateToken } from "../middleware/auth.middleware";
+
+const router = Router();
+
+// get user
+router.get("/user", authenticateToken, async (req, res) => {
+  try {
+    const user_id = (<any>req).user.user_id;
+    const listOfUser: IUser[] = await db.execute(
+      `SELECT * FROM User WHERE user_id = ?`,
+      [user_id]
+    );
+
+    return res.json(listOfUser);
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+});
+
+export { router as user };
