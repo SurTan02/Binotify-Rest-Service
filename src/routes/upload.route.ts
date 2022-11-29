43da8caa78966e5fdb7e43625ecfbadbc6244369
Suryanto Tan <suryanto.tan5@gmail.com>
2022-11-30 00:00:54 +0700

    feat: fix url for uploaded file

--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -8,9 +8,8 @@ const router = Router();
 
 router.post('/upload', authenticateToken, (req, res) => {
     
-
     if (!req.files){
-        return res.json({msg: "Please upload mp4 file"});
+      return res.json({msg: "Please upload mp4 file"});
     }
 
     let uploadFile = (<any>req).files.file;
@@ -23,11 +22,11 @@ router.post('/upload', authenticateToken, (req, res) => {
     
     // const md5 = uploadFile.md5();
     const saveAs = `${name}`;
-    const url = `src/public/${saveAs}`
+    const url = `public/${saveAs}`
 
-    uploadFile.mv( url, function(err: any) {
+    uploadFile.mv( "src/" + url, function(err: any) {
       if (err) {
-        return res.status(500).send(err);
+        return res.status(500).json(err);
       }
       return res.status(200).json({ status: 'uploaded', saveAs , url });
     });
