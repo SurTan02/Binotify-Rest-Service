35809ee0764eefaf5ee56f5ea02228b724deb12c
Suryanto Tan <suryanto.tan5@gmail.com>
2022-12-01 15:11:31 +0700

    feat: add endpoint to get admin email

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,19 +1,30 @@
 import { IUser } from "./../model/user.model";
 import { Router } from "express";
 import * as db from "../db/connection";
-import { authenticateToken } from "../middleware/auth.middleware";
 
 const router = Router();
 
 // get user
 router.get("/user",  async (req, res) => {
   try {
-    const user_id = (<any>req).user.user_id;
+    // const user_id = (<any>req).user.user_id;
     const listOfUser: IUser[] = await db.execute(
-      `SELECT * FROM User WHERE user_id = ?`,
-      [user_id]
+      `SELECT * FROM User WHERE isAdmin=0`,
+      []
     );
 
+    return res.json(listOfUser);
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+});
+
+router.get("/admin",  async (req, res) => {
+  try {
+    const listOfUser = await db.execute(
+      `SELECT email FROM User WHERE isAdmin=1`,
+      []
+    );
     return res.json(listOfUser);
   } catch (e) {
     return res.sendStatus(500);
